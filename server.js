596decadf2892bb9a8142d641a75e0a65277ef4b
Divya Singh <divyasingh5028@gmail.com>
2025-02-04 16:10:00 +0530

    Adding database

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const app = express();
 const port = 3000;
-app.listen(port,()=>{
-    console.log(`Server is running on Port ${port}`)
-})
 
-app.get('/abc',(req,res)=>{
+// MongoDB connection string (replace with your own credentials)
+const url = "mongodb+srv://divyasingh5028:lucygreat@asap.6ch51.mongodb.net/?retryWrites=true&w=majority&appName=ASAP";
+
+// Connect to MongoDB
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect()
+  .then(() => {
+    console.log("Connected to MongoDB Atlas!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
+
+// Define routes
+app.get('/abc', (req, res) => {
     res.send("Hello World!")
-})
+});
 
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+});
